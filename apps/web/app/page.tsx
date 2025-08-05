2a25e096e8f8aaa78d7961603fc14550244db0dd
Bakate <bakateba@gmail.com>
2025-08-05 07:21:38 +0200

    feat(convex): integrate Convex backend with user management functionality

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,21 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+import { Button } from "@workspace/ui/components/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getAllUsers);
+  const addUser = useMutation(api.users.addUser);
+  console.log(users);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">Hello World from app/web</h1>
         <Button size="sm">Button</Button>
+        <ul>{users?.map((user) => <li key={user._id}>{user.name}</li>)}</ul>
+
+        <Button onClick={() => addUser()}>Add User</Button>
       </div>
     </div>
-  )
+  );
 }
---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,18 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const getAllUsers = query({
+  args: {},
+  handler: async (ctx) => {
+    return ctx.db.query("users").collect();
+  },
+});
+
+export const addUser = mutation({
+  args: {},
+  handler: async (ctx) => {
+    return ctx.db.insert("users", {
+      name: `User Bakate ${Math.ceil(Math.random() * 100).toFixed(2)}`,
+    });
+  },
+});
